7c075b08773097995f4e422002e198405da34ae5
ved3any <98474461+ved3any@users.noreply.github.com>
2025-05-24 17:13:53 +0530

    Add files via upload

---
+++ login/script.js
@@ -0,0 +1,105 @@
+const loginForm = document.getElementById('login-form');
+const registerForm = document.getElementById('register-form');
+const switchToRegister = document.getElementById('switch-to-register');
+const switchToLogin = document.getElementById('switch-to-login');
+const authSection = document.getElementById('auth');
+const formTitle = document.getElementById('form-title');
+const WEB_URL = "Website_Progs/Crick Player"
+const API_URL = "http://localhost:3000"
+
+addEventListener("DOMContentLoaded", (d) => {
+val = getCookie("loggedin");
+if (val === "true"){
+    window.location.pathname = WEB_URL + "/dashboard";
+}else {
+
+}
+});
+
+switchToRegister.addEventListener('click', (e) => {
+  e.preventDefault();
+  loginForm.style.display = 'none';
+  registerForm.style.display = 'flex';
+  formTitle.textContent = 'Register';
+});
+
+switchToLogin.addEventListener('click', (e) => {
+  e.preventDefault();
+  loginForm.style.display = 'flex';
+  registerForm.style.display = 'none';
+  formTitle.textContent = 'Login';
+});
+
+loginForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  emailvalue = document.getElementById("lgn-email").value;
+  passwordvalue = document.getElementById("lgn-pass").value;
+
+  fetch(API_URL + '/login', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json' 
+    },
+    body: JSON.stringify({email : emailvalue, password : passwordvalue})
+  })
+  .then( res => {
+    if (res.status == 401) {
+      alert("Email Or Password Is Incorrect!");
+    }
+    else if (!res.ok){
+      throw new Error("Login Failed");
+    }
+    else {
+      return res;
+    }
+  })
+  .then(res => res.text())
+  .then(id => {
+    CreateOrUpdateCookie("loggedin","true",1);
+    CreateOrUpdateCookie("id",id,1);
+    window.location.pathname = WEB_URL + '/dashboard';
+  })
+  .catch(err => alert(err.message));
+});
+
+registerForm.addEventListener('submit', (e) => {  
+  e.preventDefault();
+  usernamevalue = document.getElementById("name").value;
+  emailvalue = document.getElementById("email").value;
+  passwordvalue = document.getElementById("pass").value;
+
+  fetch(API_URL + '/register', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({'username' : usernamevalue, 'email' : emailvalue, 'password' : passwordvalue})
+  })
+  .then(res => {
+    if (!res.ok) {
+      throw new Error('Registration Failed - Maybe Email Or Username is Already Registered');
+    }
+    return res
+  })
+  .then(res => res.text())
+  .then(id => {
+      CreateOrUpdateCookie("loggedin","true",1);
+      CreateOrUpdateCookie("id",id,1);
+      window.location.pathname = WEB_URL + '/dashboard';
+    })
+  .catch(err => alert(err.message));
+});
+
+function getCookie(name) {
+  const cookies = document.cookie.split("; ");
+  for (let c of cookies) {
+    const [key, val] = c.split("=");
+    if (key === name) return val;
+  }
+  return null;
+}
+
+function CreateOrUpdateCookie(name, value, days = 7) {
+  const expires = new Date(Date.now() + days * 864e5).toUTCString(); // 864e5 = 86400000 ms = 1 day
+  document.cookie = `${name}=${value}; expires=${expires}; path=/; Secure`;
+}
