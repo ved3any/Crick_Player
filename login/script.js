a8405d7547be53aa2ae3f818e3b520d61ac93ac9
ved3any <98474461+ved3any@users.noreply.github.com>
2025-06-04 13:59:47 +0530

    Update script.js

--- login/script.js
+++ login/script.js
@@ -5,7 +5,7 @@ const switchToLogin = document.getElementById('switch-to-login');
 const authSection = document.getElementById('auth');
 const formTitle = document.getElementById('form-title');
 const WEB_URL = ""
-const API_URL = "http://localhost:3000"
+const API_URL = "http://localhost:3000/api"
 
 addEventListener("DOMContentLoaded", (d) => {
 val = getCookie("loggedin");
